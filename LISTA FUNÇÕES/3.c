d5221dc612d4ece9c00c246493d61a223c833f79
PauloEncarn <99846206+PauloEncarn@users.noreply.github.com>
2022-04-06 18:12:08 +0000

    lista

--- LISTA FUNÇÕES/3.c
+++ LISTA FUNÇÕES/3.c
@@ -2,7 +2,7 @@
 -Instituto Federal da Bahia-
 Curso: Análise e Desenvolvimento de Sistemas
 Lista de Exercícios 1 - Funções
-Aluno: Jefferson Barreto
+Aluno: Paulo Henrique dos santos da Encarnação
 Questão 3: 
     Faça um programa que tenha uma função que recebe um número inteiro e retorna o fatorial
 deste número. Esta função deve ainda verificar se é possível calcular o fatorial, se não for, ela deve
@@ -13,17 +13,37 @@ usuário e imprimir o fatorial dele, ou se não é possível calcular o fatorial
 #include <stdio.h>
 #include <stdlib.h>
 
-int fatorial (int x){
+int fatorial(int a){
+int fat=1;
 
-int fat;
+if (a<0)
+{
+  return -1;
 
-  for(fat=1;x>1;x++)
-    {
-      fat=fat*x;
-    }
+}
 
-  if (x<0)
-  {
-    return -1;
+else
+
+  for ( ; a>0; fat *=a--)
+    return fat;
   }
+
+int main(){
+
+int resultado, num1;
+
+printf ("-----FATORIAL-------");
+printf ("\nDigite o numero:");
+scanf("%d",&num1);
+
+resultado = fatorial(num1);
+
+if (num1<0)
+{
+  printf ("nao tem ");
+}
+else 
+  printf("%d",resultado);
+
+return 0;
 }
